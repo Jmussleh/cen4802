ca2c407378c1559d7e497811f3ba0705c1293d2d
jmussleh <jmussleh@gmail.com>
2025-08-23 22:42:09 -0400

    Added javadoc comments to Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,6 +3,11 @@ which accepts n as a parameter. This class also contains a main method that
 calls the first method.
  */
 public class Fibonacci {
+    /**
+     * Calculates the nth term of the Fibonacci sequence
+     * @param n, the position in the Fibonacci sequence
+     * @return the Fibonacci number at position n
+     */
     //Recursive method for Fibonacci sequence
     public static int Fibonacci(int n) {
         if (n <= 1) {
@@ -11,6 +16,12 @@ public class Fibonacci {
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
     //Main method
+
+    /**
+     * Main method that calls the Fibonacci method and
+     * prints the message with the nth term of the sequence
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         int n = 10;
         int result = Fibonacci(n);
